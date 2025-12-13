1fd219ae664d052798f859721d9b2a433b59038e
Abby Lossa <abby.lossa@gmail.com>
2025-12-13 17:19:35 -0600

    Add social links (update LinkedIn URL)

---
+++ src/lib/socialLinks.ts
@@ -0,0 +1,6 @@
+export const SOCIAL_LINKS = {
+  facebook: 'https://www.facebook.com/Elvis-Lossa-for-Congress-TX-31',
+  twitter: 'https://x.com/Elvis-for-TX-31',
+  instagram: 'https://www.instagram.com/Elvis-for-Congress',
+  linkedin: 'https://www.linkedin.com/in/elvis-a-lossa/'
+};
